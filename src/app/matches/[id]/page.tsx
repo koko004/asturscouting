2087976b14ad3ba4dfa7dfcf086c9328cb3c3bc9
Miguel Prieto <koko004@gmail.com>
2025-09-20 09:08:34 +0000

    Como va a ser una aplicacion movil el panel de jugadores evaluados prefi

--- src/app/matches/[id]/page.tsx
+++ src/app/matches/[id]/page.tsx
@@ -15,7 +15,7 @@ export default function MatchPage() {
   const matchId = typeof params.id === 'string' ? params.id : '';
   const match = matches.find((m) => m.id === matchId);
 
-  const [players, setPlayers] = useState<Player[]>(initialPlayers);
+  const [players, setPlayers] = useState<Player[]>([]);
   const [isFormOpen, setIsFormOpen] = useState(false);
   const [selectedPlayer, setSelectedPlayer] = useState<Player | null>(null);
 
@@ -71,24 +71,20 @@ export default function MatchPage() {
   };
 
   return (
-    <div className="flex h-[calc(100vh-8rem)] flex-col gap-8">
+    <div className="flex flex-col gap-8">
        <PageHeader
         title={`${match.homeTeam.name} vs ${match.awayTeam.name}`}
         description={`Scouting Report for ${match.competition}`}
       />
-      <div className="grid flex-1 gap-6 md:grid-cols-3 lg:grid-cols-4">
-        <div className="flex flex-col gap-6 md:col-span-2 lg:col-span-3">
-          <InteractiveField onPlayerClick={handleSelectPlayerFromField} />
-          <MatchNotes />
-        </div>
-        <div className="md:col-span-1 lg:col-span-1">
-          <PlayerList 
-            players={players} 
-            onPlayerUpdate={handlePlayerUpdate}
-            onEditPlayer={handleEditPlayer}
-            onAddNewPlayer={handleAddNewPlayer} 
-          />
-        </div>
+      <div className="flex flex-col gap-6">
+        <InteractiveField onPlayerClick={handleSelectPlayerFromField} />
+        <PlayerList 
+          players={players} 
+          onPlayerUpdate={handlePlayerUpdate}
+          onEditPlayer={handleEditPlayer}
+          onAddNewPlayer={handleAddNewPlayer} 
+        />
+        <MatchNotes />
       </div>
       <PlayerForm
         isOpen={isFormOpen}
